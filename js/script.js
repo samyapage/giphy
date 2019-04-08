e535709bec4195989975537c8a7fb1343c36172b
Samya page <samyapage28@gmail.com>
2019-04-08 22:27:41 +0000

    ajaxs

--- js/script.js
+++ js/script.js
@@ -4,8 +4,13 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    
+    var searchTerm = $('#search-term').val();
+    
+    $.ajax({url: "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC", success: function(response){
+        // $("#div1").html(result);
+        console.log(response);
+    }});
+    
 });
 
